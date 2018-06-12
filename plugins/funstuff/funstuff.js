b9e6dfe1a27324014da9fdee6167ddecdeb6378b
Daniel Coady <daniel.coady@outlook.com.au>
2018-06-12 15:02:42 +1000

    Added data validation to roll and funstuff plugins

--- plugins/funstuff/funstuff.js
+++ plugins/funstuff/funstuff.js
@@ -8,18 +8,26 @@ exports.commands = ["cowsay", "figlet"];
 exports.init = function(client, config, _) {}
 exports["cowsay"] = {
     process: function(bot, message, suffix) {
-        message.channel.send("```" + cowsay.say({text: suffix}) + "```");
+        if (suffix.length > 30) {
+            message.channel.send("Please send a message that is 30 characters or less");
+        } else {
+            message.channel.send("```" + cowsay.say({text: suffix}) + "```");
+        }
     }
 }
 
 exports["figlet"] = {
     process: function(bot, message, suffix) {
-        figlet(suffix, function(err, data) {
-            if (err) {
-                message.channel.send("Something fucked up :(");
-            } else {
-                message.channel.send("```" + data + "```");
-            }
-        })
+        if (suffix.length > 10) {
+            message.channel.send("Please send a message that is 10 characters or less");
+        } else {
+            figlet(suffix, function(err, data) {
+                if (err) {
+                    message.channel.send("Something fucked up :(");
+                } else {
+                    message.channel.send("```" + data + "```");
+                }
+            });
+        }
     }
 }
--- plugins/roll/roll.js
+++ plugins/roll/roll.js
@@ -12,11 +12,30 @@ exports["roll"] = {
         if (input.length != 2)
             return message.channel.send("Invalid format! Make sure you use it like `" + config.prefix + "roll [#/dice]d[#/faces]`");
 
-        numberRolled = suffix.split("d")[0];
-        faceCount = suffix.split("d")[1];
+        numberRolled = parseInt(suffix.split("d")[0]);
+        faceCount = parseInt(suffix.split("d")[1]);
+        errMsg = "";
+
+        if (numberRolled > 10 || numberRolled < 1) {
+            errMsg += "Please only roll between 1 and 10 dice";
+        }
+
+        if (faceCount > 120 || faceCount < 2) {
+            if (errMsg != "") {
+                errMsg += " and make sure the dice only has 2-120 faces";
+            } else {
+                errMsg += "Please make sure your dice only has 2-120 faces";
+            }
+        }
+
+        if (errMsg != "") {
+            message.channel.send(errMsg);
+            return;
+        }
+
         var result = "";
 
-        for (i = 0; i < parseInt(numberRolled); i++) {
+        for (i = 0; i < numberRolled; i++) {
             result += "Roll " + (i + 1) + ": " +
                 ( 1 + Math.floor(Math.random() * Math.floor(faceCount))) + "\n";
         }
