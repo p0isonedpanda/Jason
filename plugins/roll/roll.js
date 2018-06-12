a8bd0575cfe67d3c1987230a4d53721f3fae6353
p0isonedpanda <daniel.coady@outlook.com.au>
2018-06-13 00:27:31 +1000

    Added modifiers to dice rolls

--- plugins/roll/roll.js
+++ plugins/roll/roll.js
@@ -12,8 +12,20 @@ exports["roll"] = {
         if (input.length != 2)
             return message.channel.send("Invalid format! Make sure you use it like `" + config.prefix + "roll [#/dice]d[#/faces]`");
 
+        // The format for a dice roll argument should be [num of dice]d[num of faces]
+        // There is also an optional modifier we can place at the end to add or subtract from the rolled value
         numberRolled = parseInt(suffix.split("d")[0]);
-        faceCount = parseInt(suffix.split("d")[1]);
+        // Check if there is a modifier for this roll
+        if (suffix.split("d")[1].split("+").length == 2) {
+            faceCount = parseInt(suffix.split("d")[1].split("+")[0]);
+            modifier = parseInt(suffix.split("d")[1].split("+")[1]);
+        } else if (suffix.split("d")[1].split("-").length == 2) {
+            faceCount = parseInt(suffix.split("d")[1].split("-")[0]);
+            modifier = parseInt(suffix.split("d")[1].split("-")[1]) * -1;
+        } else { // If there is no modifier present
+            faceCount = parseInt(suffix.split("d")[1]);
+            modifier = 0;
+        }
         errMsg = "";
 
         if (numberRolled > 10 || numberRolled < 1) {
@@ -37,7 +49,7 @@ exports["roll"] = {
 
         for (i = 0; i < numberRolled; i++) {
             result += "Roll " + (i + 1) + ": " +
-                ( 1 + Math.floor(Math.random() * Math.floor(faceCount))) + "\n";
+                (1 + Math.floor(Math.random() * Math.floor(faceCount)) + modifier) + " (mod: " + modifier + ")" + "\n";
         }
         if (result == "") result = "Sorry, looks like you're trying to make me roll something I can't";
         message.channel.send(result);
